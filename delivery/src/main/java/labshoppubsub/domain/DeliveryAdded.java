c8bd12841b647bb742081f08d8ce92297464ff6e
Dongjun Park <yh02195@gmail.com>
2024-03-14 13:52:05 +0900

    commit

---
+++ delivery/src/main/java/labshoppubsub/domain/DeliveryAdded.java
@@ -0,0 +1,24 @@
+package labshoppubsub.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import labshoppubsub.domain.*;
+import labshoppubsub.infra.AbstractEvent;
+import lombok.*;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class DeliveryAdded extends AbstractEvent {
+
+    private Long id;
+
+    public DeliveryAdded(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryAdded() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
